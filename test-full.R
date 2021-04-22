b98d8b85046c56e31fcf3ed0fa4668231f3d0fb4
Kai Okada <kaiokada@MacBook-Air.local>
2021-04-21 21:01:42 -0400

    added more modular seed generation and prediction code

--- test-full.R
+++ test-full.R
@@ -20,54 +20,45 @@ set_config_param(param = "report_folder",
                  value = "/Users/kaiokada/Desktop/Research/platelet_data_full/Blood_Center_Reports")
 
 config <- set_config_param(param = "data_folder",
-                           value = "/Users/kaiokada/Desktop/Research/platelet_data_full/Blood_Center")
-
-
-## This is for creating the seed dataset
-cbc <- process_all_cbc_files(data_folder = config$data_folder,
-                             cbc_abnormals = config$cbc_abnormals,
-                             cbc_vars = config$cbc_vars)
-
-census <- process_all_census_files(data_folder = config$data_folder,
-                                   locations = config$census_locations)
-
-transfusion <- process_all_transfusion_files(data_folder = config$data_folder)
-config
-## Save the SEED dataset so that we can begin the process of building the model.
-## This gets us to date 2018-04-09-08-01-54.txt, so we save data using that date
-saveRDS(list(cbc = cbc, census = census, transfusion = transfusion),
-        file = file.path(config$output_folder,
-                         sprintf(config$output_filename_prefix, "2018-04-12")))
-
-## End of Seed Creation
-
-## Incremental Data
-config <- set_config_param(param = "data_folder",
                            value = "/Users/kaiokada/Desktop/Research/platelet_data_full/Blood_Center_inc")
 
 config$history_window <- 200
 
-# Predict from 2018-04-13 all the way to 2020-01-02
-start_date <- as.Date("2018-10-10")
-end_date <- as.Date("2019-04-15")
+# Predict 106 days out from January 1, 2019
+pred_start_date <- as.Date("2019-01-01")
+pred_end_date <- pred_start_date + 104
+seed_start_date <- pred_start_date - 201
+seed_end_date <- pred_start_date - 1
+all_seed_dates <- seq.Date(from = seed_start_date, to = seed_end_date, by = 1)
 
-all_dates <- seq.Date(from = start_date, to = end_date, by = 1)
-
-prediction <- NULL
-
-for (i in seq_along(all_dates)) {
-  date_str <- as.character(all_dates[i])
-  print(date_str)
-  
-  result <- predict_for_date(config = config, date = date_str)
-  
-  print(result)
-  prediction <- rbind(prediction,result)
+# Process Seed Data over specified date range
+# (This likely will not work on files that contain information for multiple days - pre 4/2018)
+seed_data <- list(cbc = NULL, census = NULL, transfusion = NULL, inventory = NULL)
+for (i in seq_along(all_seed_dates)) {
+  date_str <- as.character(all_seed_dates[i])
+  data_single_day <- process_data_for_date(config = config, date = date_str)
+  seed_data$cbc %<>% rbind(data_single_day$cbc)
+  seed_data$census %<>% rbind(data_single_day$census)
+  seed_data$transfusion %<>% rbind(data_single_day$transfusion)
+  seed_data$inventory %<>% rbind(data_single_day$inventory)
 }
 
+# Save the seed dataset
+saveRDS(object = seed_data,
+        file = file.path(config$output_folder,
+                         sprintf(config$output_filename_prefix, as.character(seed_end_date))))
+
+# Predict for specified date range
+all_pred_dates <- seq.Date(from = pred_start_date, to = pred_end_date, by = 1)
+prediction <- NULL
+for (i in seq_along(all_pred_dates)) {
+  date_str <- as.character(all_pred_dates[i])
+  result <- predict_for_date(config = config, date = date_str)
+  prediction <- rbind(prediction,result)
+}
 ggplot(data=prediction) + geom_line(aes(x=date, y=t_pred))
 
-# Retrieve true transfusion data from the files
+# Retrieve true transfusion data from the files (this fetches data from all files in folder)
 process_all_transfusion_files_no_reports <- function(data_folder,
                                                      pattern = "LAB-BB-CSRP-Transfused*") {
   fileList <- list.files(data_folder, pattern = pattern , full.names = TRUE)
@@ -80,7 +71,6 @@ process_all_transfusion_files_no_reports <- function(data_folder,
     dplyr::arrange(date)
 }
 
-# 
 transfusion_inc <- process_all_transfusion_files_no_reports(data_folder = config$data_folder)
 transfusion_dates <- sapply(transfusion_inc$date, function(x) {as.character(x, format = "%Y-%m-%d")})
 transfusion_used <- as.numeric(transfusion_inc$used)
@@ -88,6 +78,8 @@ plt_used_df <- data.frame(transfusion_dates, transfusion_used)
 plt_used_tbl <- as_tibble(plt_used_df[1:nrow(plt_used_df),])
 plt_used_tbl$transfusion_dates %<>% as.Date
 names(plt_used_tbl) = c("date", "d_true")
+plt_used_tbl %<>% filter(date >= pred_start_date) %<>% filter(date <= pred_end_date)
+
 prediction %>% left_join(plt_used_tbl, by="date") -> pred_and_true
 pred_and_true %>%
   mutate(dlead1 = lead(pred_and_true$d_true, 1)) %>%
@@ -104,12 +96,10 @@ ggplot(data=pred_and_true[1:(nrow(pred_and_true)-2),]) +
   geom_line(aes(x=date, y=surplus)) 
 
 # Attempt to generate prediction table (does not work - actually platelet usage returns NA )
-full_pred_table <- build_prediction_table(config, start_date, end_date)
-full_pred_table
+full_pred_table <- build_prediction_table(config, pred_start_date, pred_end_date)
 write.csv(full_pred_table, "predictions.csv")
 
 # Collect model coefficients over all output files (only need one set per week)
-config$output_folder
 output_files <- list.files(path = config$output_folder,
                            pattern = paste0("^",
                                             substring(config$output_filename_prefix, first = 1, last = 10)),
@@ -123,13 +113,9 @@ for (i in seq(1, length(output_files), 7)) {
 }
 coef.df <- data.frame(model_coefs)
 
-# Plot time-varying coefficients by week
-nrow(coef.df) * 7
-start_date <- as.Date("2018-10-10")
-end_date <- as.Date("2019-04-14")
-
-all_dates <- seq.Date(from = start_date, to = end_date, by = 7)
-coef.df$date = all_dates
+# Plot time-varying coefficients by week (Lambda is 0 between these dates)
+all_coef_dates <- seq.Date(from = pred_start_date, to = pred_end_date, by = 7)
+coef.df$date = all_coef_dates
 coef.tbl <- as.tibble(coef.df)
 coef.tbl %>% relocate(date) -> coef.tbl
 coef.tbl %>% filter(date < as.Date("2019-11-10") & date > as.Date("2019-06-01"))
@@ -193,3 +179,4 @@ ggplot(data=coef.tbl) +
 
 # Lambda
 ggplot(data=coef.tbl) + geom_line(mapping=aes(x=date, y=lambda, col='lambda'))
+
